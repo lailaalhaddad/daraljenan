1fba6deaae44a5537240760a841e0f929e9ef66a
lailaalhaddad <lalhaddad95@gmail.com>
2021-02-16 18:54:26 +0300

    till task two

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import img from "./image/daraljenan.JPG";
+import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Dar Aljenan Resturent</h1>
+        <h2>Served all kind of food, but our specialty is grape leaves.</h2>
+        <img src={img} className="App-front" alt="front" />
       </header>
+      <div>
+        <ProductList />
+      </div>
     </div>
   );
 }
